0930f7aabcadb149df6da72392482f0683e842c1
Vasiliev Sergei <sergei.binp@gmail.com>
2023-04-23 00:58:24 +0500

    6th commit: мои первые методы на С#

--- Practicies/Program.cs
+++ Practicies/Program.cs
@@ -40,8 +40,14 @@ void FillArray(int[] collection){
 void PrintArray(int[] col){
     int count = col.Length;
     int position = 0;
+    while (position < count){
+        Console.WriteLine(col[position]);
+        position++;
+    }
 
 }
 
 int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
 
